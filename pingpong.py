239eb5af09684d9fff78c0132ab84408090ac523
Art1les <93582988+Art1les@users.noreply.github.com>
2022-04-09 17:28:38 +0500

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,58 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+   def __init__(self, player_image, player_x, player_y, player_speed):
+       super().__init__()
+       self.image = transform.scale(image.load(player_image), (65, 65))
+       self.speed = player_speed
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+       
+class St_player(GameSprite):
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+class Nd_player(GameSprite):
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+class Ball(GameSprite):
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < win_height - 80:
+           self.rect.y += self.speed
+
+
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load("bg.jpg"), (win_width, win_height))
+
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    if finish != True:
+        window.blit(background,(0, 0))
+    display.update()
+    clock.tick(FPS)
